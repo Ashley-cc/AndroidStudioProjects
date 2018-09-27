c7b50a3837af58957379bba5da7a93437b74d14e
Ashley-cc <clf1091058214@163.com>
2018-09-27 18:50:05 +0800

    1

--- HelloWorld/app/src/main/java/com/example/clf/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/example/clf/helloworld/MainActivity.java
@@ -10,6 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
         Log.d("MainActivity","OnCreate execute");
 
     }
