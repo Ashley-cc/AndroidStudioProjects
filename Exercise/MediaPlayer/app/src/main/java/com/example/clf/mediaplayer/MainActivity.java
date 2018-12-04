4ab6e8905d1c3cb05527796fea2dfab7a0f64697
Ashley-cc <clf1091058214@163.com>
2018-12-04 22:24:09 +0800

    后台服务播放，播放闪退的问题也得到了解决
    播放闪退的问题是由于数据库查询获取列表可以用MediaStore.Audio.Media.EXTERNAL_CONTENT_URI获取音乐列表
    但是播放获取文件不能用；
    在这里特别感谢老师的耐心指导，解决了困扰了我很久的问题！

--- Exercise/MediaPlayer/app/src/main/java/com/example/clf/mediaplayer/MainActivity.java
+++ Exercise/MediaPlayer/app/src/main/java/com/example/clf/mediaplayer/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.clf.mediaplayer;
 
 import android.Manifest;
+import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.media.AudioManager;
@@ -10,6 +14,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -21,6 +26,7 @@ import android.widget.Button;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
@@ -35,8 +41,11 @@ public class MainActivity extends AppCompatActivity {
     private Button btn_stop;
     private ListView list;
 
-    private MediaPlayer player;
+    private Cursor cursor;
+    private String path;
+    private int status;
     private int number;//当前歌曲的序号，下标从1开始
+    private StatusChangedReceiver receiver;
 
     private static final int REQUEST_PERMISSION = 0;
     @Override
@@ -46,12 +55,41 @@ public class MainActivity extends AppCompatActivity {
         findViews();
         registerListeners();
         number=1;
+        status=MusicService.STATUS_STOPPED;
+        startService(new Intent(this,MusicService.class));
+        bindStatusChangedReceiver();
+        sendBroadcastOnCommand(MusicService.COMMAND_CHECK_IS_PLAYING);
 
-        if(ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.WRITE_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED){
+        /*if(ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.WRITE_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
         }else{
-            Toast.makeText(this,"列表初始化",Toast.LENGTH_LONG).show();
+            //initMusicList();
+            Toast.makeText(this,"列表初始化...",Toast.LENGTH_LONG).show();
+        }*/
+    }
+
+ /*   @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode){
+            case 1:
+            if(grantResults.length>0&&grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this,"权限允许",Toast.LENGTH_SHORT).show();
+               // initMusicList();
+            }else{
+                Toast.makeText(this,"拒绝权限将无法使用程序",Toast.LENGTH_SHORT).show();
+                finish();
+            }
+            break;
+            default:
         }
+       // super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }*/
+
+    private void bindStatusChangedReceiver(){
+       receiver = new StatusChangedReceiver();
+        IntentFilter filter = new IntentFilter(MusicService.BROADCAST_MUSICSERVICE_UPDATE_STATUS);
+        registerReceiver(receiver,filter);
+
     }
 
 
@@ -65,13 +103,78 @@ public class MainActivity extends AppCompatActivity {
     }
     //为显示组件注册监听器
     private void registerListeners(){
+        btn_previous.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendBroadcastOnCommand(MusicService.COMMAND_PREVIOUS);
+                //moveNumberToPrevious();
+                //play(number);
+                //btn_play.setBackground();
+            }
+        });
+     //播放
+        btn_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isPlaying()){
+                    sendBroadcastOnCommand(MusicService.COMMAND_PAUSE);
+                }else if(isPaused()){
+                    sendBroadcastOnCommand(MusicService.COMMAND_RESUME);
+                }else if(isStopped()){
+                    sendBroadcastOnCommand(MusicService.COMMAND_PLAY);
+                }
+               /* if(player!=null && player.isPlaying()){
+                    pause();
+                }else{
+                    //player.prepare();
+                    play(number);//play(number);
+                }*/
+            }
+        });
+//下一首
+        btn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*moveNumberToNext();
+                play(number);*/
+                sendBroadcastOnCommand(MusicService.COMMAND_NEXT);
+            }
+        });
+//停止
+        btn_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendBroadcastOnCommand(MusicService.COMMAND_STOP);
+               // stop();
+            }
+        });
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                number=position+2;
+                TextView textView=view.findViewById(android.R.id.text1);
+                //path=  textView.getText().toString();
+                //Log.v("abc",path);
+                sendBroadcastOnCommand(MusicService.COMMAND_PLAY);
+                //play(number);
+            }
+        });
+
 
     }
 
     protected void onResume() {
         super.onResume();
+
+       /* if(ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.WRITE_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+        }else{*/
+            initMusicList();
+        /*    Toast.makeText(this,"列表初始化...",Toast.LENGTH_LONG).show();
+        }*/
         //初始化音乐列表
-        initMusicList();
+       //initMusicList();
         if(list.getCount()==0){
             btn_previous.setEnabled(false);
             btn_play.setEnabled(false);
@@ -89,22 +192,39 @@ public class MainActivity extends AppCompatActivity {
     private void initMusicList(){
 
         Cursor cursor= getMusicCursor();
+        path=getPath(1);
        // cursor.moveToFirst();
+        //getPath(0);
         setListContent(cursor);
     }
 
     private void setListContent(Cursor musicCursor){
         CursorAdapter adapter=new SimpleCursorAdapter(this,android.R.layout.simple_list_item_2,musicCursor,new String[]{
+                //MediaStore.Audio.AudioColumns.DATA,
                 MediaStore.Audio.AudioColumns.TITLE,
+
                 MediaStore.Audio.AudioColumns.ARTIST},new int[]{
                 android.R.id.text1,android.R.id.text2 });
           list.setAdapter(adapter);
     }
 
+    private String getPath(int number){
+        cursor.moveToFirst();
+        for(int i=0;i<number-1;i++) {
+            cursor.moveToNext();
+        }
+        return cursor.getString(1);
+      //  Log.v("testpath", cursor.getString(1));
+        //return cursor.getString(0);
+    }
+   /* private ArrayList<String> getPath{
+
+    }*/
+
     private Cursor getMusicCursor(){
         ContentResolver resolver = getContentResolver();
-       /* File file=new File(Environment.getExternalStorageDirectory(),null);*/
-        Cursor cursor=resolver.query(/*Uri.parse(file.getPath())*//*Uri.parse("file://storage/emulated/0/kgmusic/download")*/MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,null,null,null,null);
+       //File file=new File(Environment.getExternalStorageDirectory(),null);
+        cursor=resolver.query(/*Uri.parse(file.getPath())*//*Uri.parse("file://storage/emulated/0/kgmusic/download")*/MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,null,null,null,null);
         return cursor;
     }
 
@@ -117,7 +237,7 @@ public class MainActivity extends AppCompatActivity {
             number=1;
             Toast.makeText(this,"已到达列表底端",Toast.LENGTH_LONG).show();
         }else{
-            ++number;
+            number++;
         }
     }
 
@@ -126,7 +246,86 @@ public class MainActivity extends AppCompatActivity {
             number=list.getCount();
             Toast.makeText(this,"已到达列表顶端",Toast.LENGTH_LONG).show();
         }else{
-            --number;
+            number--;
         }
     }
+
+
+
+    //发送命令，控制音乐播放
+    private void sendBroadcastOnCommand(int command){
+        Intent  intent = new Intent(MusicService.BROADCAST_MUSICSERVICE_CONTROL);
+        intent.putExtra("command",command);
+
+        //根据不同的命令，封装不同的数据
+        switch(command){
+            case MusicService.COMMAND_PLAY:
+                intent.putExtra("number",number);
+                //intent.putExtra("test",getPath(number));
+                //intent.putExtra("test",getPath(number));
+                intent.putExtra("path",getPath(number));
+            case MusicService.COMMAND_PREVIOUS:
+                moveNumberToPrevious();
+                //intent.putExtra("path",path);
+                intent.putExtra("number",number);
+                intent.putExtra("path",getPath(number));
+               // intent.putExtra("path",path);
+                break;
+            case MusicService.COMMAND_NEXT:
+                moveNumberToNext();
+                intent.putExtra("number",number);
+                intent.putExtra("path",getPath(number));
+                //intent.putExtra("path",path);
+                break;
+            case MusicService.COMMAND_PAUSE:
+            case MusicService.COMMAND_STOP:
+            case MusicService.COMMAND_RESUME:
+            default:
+                break;
+        }
+        sendBroadcast(intent);
+    }
+
+
+    //播放状态
+    //private int status;
+    //是否正在播放音乐
+    private boolean isPlaying(){
+        return status==MusicService.STATUS_PLAYING;
+    }
+    //是否暂停了播放音乐
+    private boolean isPaused(){
+        return status==MusicService.STATUS_PAUSED;
+    }
+    //是否停止状态
+    private boolean isStopped(){
+        return status==MusicService.STATUS_STOPPED;
+    }
+    public void onDestroy(){
+        if(isStopped()){
+            stopService(new Intent(this,MusicService.class));
+        }
+        super.onDestroy();
+    }
+
+    class StatusChangedReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            status=intent.getIntExtra("status",-1);
+            if(status==MusicService.STATUS_COMPLETED){
+                sendBroadcastOnCommand(MusicService.COMMAND_NEXT);
+            }
+            //updateUI(status);
+        }
+
+       /* private void updateUI(int status){
+            switch(status){
+                case MusicService.STATUS_PLAYING:
+
+            }
+        }*/
+    }
+
 }
+
---
+++ Exercise/MediaPlayer/app/src/main/java/com/example/clf/mediaplayer/MusicService.java
@@ -0,0 +1,209 @@
+package com.example.clf.mediaplayer;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.IBinder;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MusicService extends Service{
+    //播放控制命令，标识操作
+    public static final int COMMAND_UNKNOWN = -1;
+    public static final int COMMAND_PLAY = 0;
+    public static final int COMMAND_PAUSE=1;
+    public static final int COMMAND_STOP=2;
+    public static final int COMMAND_RESUME=3;
+    public static final int COMMAND_PREVIOUS=4;
+    public static final int COMMAND_NEXT=5;
+    public static final int COMMAND_CHECK_IS_PLAYING=6;
+    //播放状态
+    public static final int STATUS_PLAYING=0;
+    public static final int STATUS_PAUSED=1;
+    public static final int STATUS_STOPPED=2;
+    public static final int STATUS_COMPLETED=3;
+    //广播标识
+    public static final String BROADCAST_MUSICSERVICE_CONTROL="MusicService.ACTION_CONTROL";
+    public static final String BROADCAST_MUSICSERVICE_UPDATE_STATUS="MusicService.ACTION_UPDATE";
+    //广播接收器
+    private CommandReceiver receiver;
+    private MediaPlayer player;
+   String path= "/storage/emulated/0/kgmusic/download/群星 - Give Me Your Love Tonight.mp3";
+    String test;
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    public void onCreate(){
+        super.onCreate();
+        bindCommandReceiver();
+        player = new MediaPlayer();
+        Toast.makeText(this,"MusicService.onCreate()",Toast.LENGTH_LONG).show();
+    }
+    public void onStart(Intent intent,int startId){
+        super.onStart(intent,startId);
+    }
+
+    public void onDestroy(){
+        if(player!=null){
+            player.release();
+        }
+        super.onDestroy();
+    }
+
+
+
+
+    private void load(int number) {
+        //player=new MediaPlayer();
+        //this.player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        //this.player.setDataSource(musicUri); // 为本地的绝对路径，如/sdcard/abc/123.wav
+        //this.player.prepare();
+        //player.prepare();
+        //player.start();
+        //player.reset();
+        if(player!=null&&player.isPlaying())
+            player.release();
+        try{
+
+ /*player=new MediaPlayer();
+            File file=new File(Environment.getExternalStorageDirectory(),null)G;
+            player.setDataSource(file.getPath());
+            player.prepare();*/
+
+            Uri musicUri = Uri.withAppendedPath(
+                    MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, /*"小星星.mp3"*/""+ number);
+         //   player = MediaPlayer.create(this,musicUri);
+
+            Log.v("abc",musicUri.toString());
+            //int number = intent.getIntExtra("number",1);
+           // String path=this.getIntExtra("path",path);
+            player.reset();
+            player.setDataSource(this, Uri.parse(path));
+            Log.v("service test","11"+path);
+
+            //player.setDataSource(this, Uri.parse("/storage/emulated/0/kgmusic/download/群星 - Give Me Your Love Tonight.mp3"));
+            player.prepare();
+/*Uri.parse("file://storage/emulated/0/kgmusic/download") musicUri)
+;*/
+
+            //player.prepare();
+  /*player=new MediaPlayer();
+
+           player.reset();
+           player.setDataSource("content://media/external/audio/media");
+           player.prepareAsync();
+           player.start();*/
+            player.setOnCompletionListener(completionListener);//注册监听器
+            // player.prepare();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+//播放结束监听器
+    MediaPlayer.OnCompletionListener completionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+            if(mp.isLooping()){
+                replay();
+            }else{
+                sendBroadcastOnStatusChanged(MusicService.STATUS_COMPLETED);
+            }
+        }
+    };
+
+    private void play(int number){
+        if(player!=null && player.isPlaying()){
+            player.stop();
+        }
+        load(number);
+        player.start();
+        sendBroadcastOnStatusChanged(MusicService.STATUS_PLAYING);
+    }
+
+    private void pause(){
+        if(player.isPlaying()){
+            player.pause();
+            sendBroadcastOnStatusChanged(MusicService.STATUS_PAUSED);
+        }
+    }
+
+    private void stop(){
+        if(player!=null){
+            player.stop();
+            sendBroadcastOnStatusChanged(MusicService.STATUS_STOPPED);
+        }
+    }
+//恢复播放
+    private void resume(){
+        player.start();
+        sendBroadcastOnStatusChanged(MusicService.STATUS_PLAYING);
+    }
+//重新播放
+    private void replay(){
+        player.start();
+        sendBroadcastOnStatusChanged(MusicService.STATUS_PLAYING);
+    }
+
+   class CommandReceiver extends BroadcastReceiver{
+        public void onReceive(Context context,Intent intent){
+            //unregisterReceiver(this);
+            //获取命令
+            //String path = intent.getIntExtra("path","/storage/emulated/0/kgmusic/download/群星 - Give Me Your Love Tonight.mp3");
+            //int number = intent.getIntExtra("number",1);
+            int command = intent.getIntExtra("command",COMMAND_UNKNOWN);
+            //执行命令
+            switch (command){
+                case COMMAND_PLAY:
+                case COMMAND_PREVIOUS:
+                case COMMAND_NEXT:
+                    int number = intent.getIntExtra("number",1);
+                     path = intent.getStringExtra("path");
+                    //test=intent.getStringExtra("test");
+                  // Log.v("service test","11"+test);
+                    Toast.makeText(MusicService.this,"正在播放第"+number+"首",Toast.LENGTH_LONG).show();
+                    play(number);
+                    break;
+                case COMMAND_PAUSE:
+                    pause();
+                    break;
+                case COMMAND_STOP:
+                    stop();
+                    break;
+                case COMMAND_RESUME:
+                    resume();
+                    break;
+
+                case COMMAND_CHECK_IS_PLAYING:
+                    if(player.isPlaying()){
+                        sendBroadcastOnStatusChanged(MusicService.STATUS_PLAYING);
+                    }
+                    break;
+                case COMMAND_UNKNOWN:
+                default:
+                    break;
+            }
+        }
+   }
+
+   private void bindCommandReceiver(){
+        receiver = new CommandReceiver();
+       IntentFilter filter = new IntentFilter(BROADCAST_MUSICSERVICE_CONTROL);
+       registerReceiver(receiver,filter);
+   }
+
+   private void sendBroadcastOnStatusChanged(int status){
+        Intent intent=new Intent(BROADCAST_MUSICSERVICE_UPDATE_STATUS);
+       intent.putExtra("status",status);
+       sendBroadcast(intent);
+   }
+
+}
